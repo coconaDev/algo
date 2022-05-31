97498e7d61325bb802652d9de3cfc4e4059972e6
YuSzmiya <skyebell13rei@gmail>
2022-05-31 11:47:45 +0900

    単方向リストのDeleteの実装

--- 7/linkedlist.c
+++ 7/linkedlist.c
@@ -80,93 +80,115 @@ void Add(int value) {
     // p->next->next = NULL;
   }
 }
+
 void Insert(struct CELL *pos, int value) {
-  if (pos == NULL) {
-    struct CELL *newCell;
+  if (pos == Header) {
+    struct CELL *newCell;  // 1
     if ((newCell = (struct CELL *)malloc(sizeof(struct CELL))) == NULL) {
-      printf("メモリ領域の確保に失敗しました。¥n");
+      printf("メモリ領域の確保に失敗しました。\n");
       exit(1);
     }
     //要素を挿入。（確保した領域にデータを格納）
-    newCell->data = value;
-    newCell->next = Header;
-    Header = newCell;
-
+    newCell->data = value;   // 2
+    newCell->next = Header;  // 3
+    Header = newCell;        // 4
   } else if (pos != NULL) {
     //１．要素へのポインタpを宣言し、先頭アドレスで初期化。
-    struct CELL *p = Header;
-
+    struct CELL *p = Header;  // 1
     //２．挿入領域newCellの宣言
     struct CELL *newCell;
-
     //３．リストの指定位置（ポインタ）までリンクを辿ってポインタｐを移動
-    while (p->next != pos) {
+    while (p->next != pos) {  // 2
       p = p->next;
     }
     //４．メモリ領域を確保(malloc)し、１で宣言したポインタnewCellに代入
-    if ((newCell = (struct CELL *)malloc(sizeof(struct CELL))) == NULL) {
-      printf("メモリ領域の確保に失敗しました。¥n");
-      exit(1);
-    }
-
-    //５．nextメンバの接続を切り替える。
+    newCell = (struct CELL *)malloc(sizeof(struct CELL));  // 3
     newCell->data = value;
-    newCell->next = NULL;
-    Header = newCell;
+    //５．nextメンバの接続を切り替える。
+    newCell->next = p->next;  // 4
+    p->next = newCell;        // 5
   }
 }
-int main(void) {
-  int no = 1;
-  int v;
-  struct CELL *p;
+void Delete(struct CELL *pos) {
+    if (pos != NULL) {
+        if (pos == Header) {
+          //１．先頭が指す要素を先頭が指す要素の次の要素に修正
+          Header = Header->next;
 
-  while (no) {
-    printf("１：初期化\n");
-    printf("２：追加\n");
-    printf("３：表示\n");
-    printf("４：挿入\n");
-    printf("０：終了\n");
-    printf(">> ");
-    scanf("%d", &no);
+          //２．メモリ領域の解放
+          free(pos);
 
-    switch (no) {
+        } else {
+          //１．要素へのポインタpを宣言し、先頭アドレスで初期化
+          struct CELL *p = Header;
+
+          //２．リストの指定位置までリンクを辿って移動
+          while (p->next != pos){
+              p = p->next;
+          }
+
+          //３．ポインタｐのnextメンバが指す要素をポインタｐのnextメンバが指す要素の次の要素に修正
+          p->next = pos->next;
+
+          //４．メモリ領域の解放
+          free(pos);
+        }
+    }
+}
+  int main(void) {
+    int no = 1;
+    int v;
+    struct CELL *p;
+
+    while (no) {
+      printf("１：初期化\n");
+      printf("２：追加\n");
+      printf("３：表示\n");
+      printf("４：挿入（先頭）\n");
+      printf("5：削除\n");
+      printf("０：終了\n");
+      printf(">> ");
+      scanf("%d", &no);
+
+      switch (no) {
         case 1:
-            printf("初期化\n");
-            Init();
-            break;
+          printf("初期化\n");
+          Init();
+          break;
         case 2:
-            printf("追加\n");
-            printf("追加する値>> ");
-            scanf("%d", &v);
-            Add(v);
-            break;
+          printf("追加\n");
+          printf("追加する値>> ");
+          scanf("%d", &v);
+          Add(v);
+          break;
         case 3:  //連結リストの状態を表示
-            printf("要素数：%d\n", Count());
-            printf("リストの状態\n");
-            for (p = Header; p != NULL; p = p->next) {
-              printf("[%d : ・-]-> ", p->data);
-            }
-            printf("NULL\n");
-            break;
+          printf("要素数：%d\n", Count());
+          printf("リストの状態\n");
+          for (p = Header; p != NULL; p = p->next) {
+            printf("[%d : ・-]-> ", p->data);
+          }
+          printf("NULL\n");
+          break;
         case 4:
-            printf("挿入\n");
-            printf("挿入先>>");
-            scanf("%d", &no);
-
-            printf("追加する値>>");
-            scanf("%d", &v);
-
-            Insert(Header, v);
-
+          printf("挿入（先頭）\n");
+          printf("追加する値>> ");
+          scanf("%d", &v);
+          Insert(Header, v);
+          break;
+        case 5:
+            printf("削除\n");
+            printf("削除する場所>>");
+            scanf("%d", p);
+            Delete(p);
             break;
 
         case 0:
-            printf("終了\n");
-            break;
+          printf("終了\n");
+          break;
         default:
-            printf("0～4を選択ください\n");
+          printf("0～3を選択ください\n");
+      }
     }
-  }
 
-  return (0);
-}
+    return (0);
+  }
