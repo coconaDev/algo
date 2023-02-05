c59b26c5ae1bdf78a3699ffa179202bdd3e91205
YuSzmiya <mi201334@edu.okinawa-ct.ac.jp>
2023-02-05 14:50:12 +0900

    ソート項目を入力する関数

--- 28/pbl.c
+++ 28/pbl.c
@@ -21,8 +21,9 @@ struct DATA m[ELEMENTS];
 // プロトタイプ宣言
 void strageData();      // データの読み込み
 char *isCityName(); // 都市名を入力
-void serch();
-void sort();
+int search();   // マッチ
+int isItem();   // ソート項目とソート順の入力
+void selectSort();  // 選択ソート
 void show();
 
 int main(void){
@@ -39,25 +40,77 @@ int main(void){
 
     switch (input){
       case 1:
-        char *str = isCityName();
+        char *CityName = isCityName();
+        int num = search(CityName);
+        show(num);
         break;
       case 2:
-        sort();
-        show();
+        int item, sort = isItem();
+        selectSort(item, sort);
+        // show();
         break;
       default:
-        printf("数字を入力してください\n");
+        printf("\n数字を入力してください");
         break;
     } 
     printf("\n");
   }
-
   return 0;
 }
+void selectSort(int item, int sort){
+  int max = 0, i = 0;
+  int *p; // ソートする構造体メンバのポインタ
+  struct DATA temp;
 
-void serch(){
+  switch (item){  // 項目番号から構造体メンバを指定
+  case 1: p[i] = m[i].area;
+  case 2: p[i] = m[i].population;
+  case 3: p[i] = m[i].rate;
+  case 4: p[i] = m[i].age;
+  case 5: p[i] = m[i].income;
+  case 6: p[i] = m[i].unemployment;
+  default:break;
+  }
+
+  for ( i = 0; i < ELEMENTS; i++){
+    for ( int j = 0; j < ELEMENTS; j++){
+      if (max < p[j]){
+        a
+      }
+    }
+  }
 }
-void sort(){
+int isItem(){
+  int item, sort;
+  printf("------ソートする項目を選択してください------\n");
+  printf("1 : 面積順\n");
+  printf("2 : 人口\n");
+  printf("3 : 増加率\n");
+  printf("4 : 平均年齢\n");
+  printf("5 : 完全失業率\n");
+  printf("6 : 所得\n");
+  printf("コマンド：");
+  scanf("%d", &item);
+  printf("\n");
+
+  printf("--ソートする順を選択してください------\n");
+  printf("0 : 昇順\n");
+  printf("1 : 降順\n");
+  scanf("%d", &sort);
+
+  sort = (sort ? -1 : 1);
+
+  return (item, sort);
+}
+int search(char *CityName){
+  for (int i = 0; i < ELEMENTS; i++){
+    printf("%s", CityName);
+    printf("%s\n", m[i].city);
+    if (!(strcmp(m[i].city, CityName))){
+        return i;
+    }
+  }
+  return 0;
 }
 char *isCityName(){
   static char str[50];
@@ -66,11 +119,14 @@ char *isCityName(){
 
   return str;
 }
-void show(){
-  // 選択した項目を表示するようにする
-  for (int i = 0; i < ELEMENTS; i++){
-    printf("%s:%g\n", m[i].city, m[i].age);
-  }
+void show(int num){
+  printf("---------検索結果------------\n");
+  printf("%s\n", m[num].city);
+  printf("面積：%.2f\n", m[num].area);
+  printf("人口：%.2f(増加率：%.2f)\n", m[num].population, m[num].rate);
+  printf("平均年齢：%.2f\n", m[num].age);
+  printf("完全失業率：%.2f\n", m[num].unemployment);
+  printf("所得（千円）：%.2f\n", m[num].income);
 }
 void strageData(){
   char buf[300];
